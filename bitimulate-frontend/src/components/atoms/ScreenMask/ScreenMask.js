0c94b6d0c74108c3e449e3887a3032400d6e24b2
velopert <public.velopert@gmail.com>
2017-08-02 01:10:40 +0900

    initialized ScreenMask component

---
+++ bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './ScreenMask.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ScreenMask = ({visible}) => {
+  return (
+    <div className={cx('screen-mask')}>
+
+    </div>
+  );
+};
+
+export default ScreenMask;
