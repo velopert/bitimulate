35f066bb946a45071b35efff8f26d90e0ff3960f
velopert <public.velopert@gmail.com>
2017-08-04 01:03:43 +0900

    fadeIn animation

--- bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
+++ bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import styles from './ScreenMask.scss';
 import classNames from 'classnames/bind';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 const cx = classNames.bind(styles);
 
 const ScreenMask = ({visible}) => {
-  if(!visible) return null;
-
   return (
-    <div className={cx('wrapper')}>>
-      <div className={cx('screen-mask')}>
-
-      </div>
-    </div>
+    <CSSTransitionGroup
+      transitionEnterTimeout={150}
+      transitionLeaveTimeout={150}
+      transitionName={{
+        enter: cx('fade-enter'),
+        leave: cx('fade-leave')
+      }}
+    >
+      { visible && <div className={cx('screen-mask')}/> }
+    </CSSTransitionGroup>
   );
 };
 
