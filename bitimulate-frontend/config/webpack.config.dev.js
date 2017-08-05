1614d01b09047945fa1b258a87c3d89c18c20dd5
velopert <public.velopert@gmail.com>
2017-08-06 00:43:31 +0900

    fixed react-hot-loader

--- bitimulate-frontend/config/webpack.config.dev.js
+++ bitimulate-frontend/config/webpack.config.dev.js
@@ -169,11 +169,13 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
-          // This is a feature of `babel-loader` for webpack (not Babel itself).
-          // It enables caching results in ./node_modules/.cache/babel-loader/
-          // directory for faster rebuilds.
-          cacheDirectory: true,
+        plugins: [
+          'react-hot-loader/babel'
+        ],
+        // This is a feature of `babel-loader` for webpack (not Babel itself).
+        // It enables caching results in ./node_modules/.cache/babel-loader/
+        // directory for faster rebuilds.
+        cacheDirectory: true,
         },
       },
       // "postcss" loader applies autoprefixer to our CSS.
