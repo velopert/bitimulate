8f883e94150a54e03458981844d853c3dfaf2699
velopert <public.velopert@gmail.com>
2017-09-03 00:10:32 +0900

    temporarily disabled react-hot-loader

--- bitimulate-frontend/src/index.js
+++ bitimulate-frontend/src/index.js
@@ -7,23 +7,28 @@ import configureStore from 'store/configure';
 import { AppContainer } from 'react-hot-loader';
 import social from 'lib/social';
 
-window.social = social;
-
 const store = configureStore();
+
+// const render = (Component) => ReactDOM.render(
+//   (
+//     <AppContainer>
+//       <Component store={store}/>
+//     </AppContainer>
+//   ), 
+//   document.getElementById('root')
+// );
 
 const render = (Component) => ReactDOM.render(
   (
-    <AppContainer>
-      <Component store={store}/>
-    </AppContainer>
+    <Component store={store}/>
   ), 
   document.getElementById('root')
 );
 
 render(Root);
 
-if(module.hot) {
-  module.hot.accept('./Root', () => render(Root))
-}
+// if(module.hot) {
+//   module.hot.accept('./Root', () => render(Root))
+// }
 
 registerServiceWorker();
