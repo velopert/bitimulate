1a2b19fcc51f3a543c2e10e9fad589c37f8a94a0
velopert <public.velopert@gmail.com>
2017-11-04 01:05:51 +0900

    disable witCredentials in poloniex API

--- bitimulate-frontend/src/lib/api/poloniex.js
+++ bitimulate-frontend/src/lib/api/poloniex.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 
-export const getOrderBook = (currencyPair) => axios.get(`https://poloniex.com/public?command=returnOrderBook&currencyPair=${currencyPair}&depth=20`);
-export const getTradeHistory = ({currencyPair, start, end}) => axios.get(`https://poloniex.com/public?command=returnTradeHistory&currencyPair=${currencyPair}${start ? `&start=${start}&end=${end}` : ''}`)
+const poloAxios = axios.create({
+  withCredentials: false
+});
+
+export const getOrderBook = (currencyPair) => poloAxios.get(`https://poloniex.com/public?command=returnOrderBook&currencyPair=${currencyPair}&depth=20`);
+export const getTradeHistory = ({currencyPair, start, end}) => poloAxios.get(`https://poloniex.com/public?command=returnTradeHistory&currencyPair=${currencyPair}${start ? `&start=${start}&end=${end}` : ''}`)
